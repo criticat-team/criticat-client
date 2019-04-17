cfc792dd4d08dbfc5d03d67b4c650083e2c28f1b
Miquel Romero Sanfeliu <miquelromero@users.noreply.github.com>
2019-04-17 11:02:32 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         criticat-client
       </h1>
       <h2 class="subtitle">
-        My striking Nuxt.js project
+        My striking Nuxt.js project!
       </h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green"
