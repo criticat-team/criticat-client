b56ee8a817424aff1649cbe5976da2f16bc32811
Miquel Romero Sanfeliu <miquelromero@users.noreply.github.com>
2019-04-18 20:07:02 +0200

    Test PR
    Ignore me

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
           <p>
             Vuetify is a progressive Material Design component framework for
             Vue.js. It was designed to empower developers to create amazing
-            applications.
+            applications!
           </p>
           <p>
             For more information on Vuetify, check out the
