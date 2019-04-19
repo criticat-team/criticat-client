84dd462185c76ddb7e4777c412a78aa4627d7ec0
miquelromero <miquelromero@outlook.com>
2019-04-19 22:48:24 +0200

    Move menu items to toolbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" fixed app>
+    <v-navigation-drawer v-if="mobile" v-model="drawer" fixed app>
       <v-list>
         <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router>
           <v-list-tile-action>
@@ -12,9 +12,29 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer" />
+    <v-toolbar primary dark color="primary" fixed app flat>
+      <v-toolbar-side-icon v-if="mobile" @click="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-tabs
+        v-if="!mobile"
+        icons-and-text
+        dark
+        centered
+        color="transparent"
+        height="84"
+      >
+        <v-tabs-slider
+          color="primary"
+          class="tabs-slider-arrow"
+        ></v-tabs-slider>
+        <v-tab v-for="(item, i) in items" :key="i" ripple :to="item.to" router>
+          {{ item.title }}
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-tab>
+      </v-tabs>
+      <v-spacer />
+      <v-btn>a</v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
@@ -30,6 +50,7 @@ import categories from '@/assets/js/categories'
 export default {
   data() {
     return {
+      mobile: false,
       drawer: false,
       items: categories.map(({ icon, title, id }) => {
         return {
@@ -48,3 +69,15 @@ export default {
   }
 }
 </script>
+
+<style lang="stylus">
+.tabs-slider-arrow {
+  width: 0 !important;
+  height: 0 !important;
+  background-color: transparent !important;
+  border-left: 10px solid transparent !important;
+  border-right: 10px solid transparent !important;
+  border-top: 10px solid;
+  margin: 0 auto;
+}
+</style>
