f6eb562b3ed6b6d39e82de043225f6d2fd47ba7f
Miquel Romero Sanfeliu <miquelromero@outlook.com>
2020-04-19 19:40:08 +0200

    Small refactor

--- src/use/scroll-state.ts
+++ src/use/scroll-state.ts
@@ -1,7 +1,7 @@
 import { computed, onMounted, Ref, ref, onUnmounted } from '@vue/composition-api';
 import Vue from 'vue';
 
-export function useScrollState(container: Ref<HTMLElement>) {
+export function useScrollState(target: Ref<HTMLElement | null>) {
   const scrollPosition: Ref<number> = ref(null);
   const scrollWidth: Ref<number> = ref(null);
   const clientWidth: Ref<number> = ref(null);
@@ -11,16 +11,16 @@ export function useScrollState(container: Ref<HTMLElement>) {
   const canScrollRight = computed(() => visibleAreaEnd.value < scrollWidth.value);
 
   const updateContainerSize = () => {
-    scrollWidth.value = container.value.scrollWidth;
-    clientWidth.value = container.value.clientWidth;
+    scrollWidth.value = target.value.scrollWidth;
+    clientWidth.value = target.value.clientWidth;
   };
 
   const updateScrollPosition = () => {
-    scrollPosition.value = container.value.scrollLeft;
+    scrollPosition.value = target.value.scrollLeft;
   };
 
   onMounted(() => {
-    container.value.addEventListener('scroll', updateScrollPosition);
+    target.value.addEventListener('scroll', updateScrollPosition);
     window.addEventListener('resize', updateContainerSize);
 
     // $nextTick is needed because vuetify adjusts the content padding after mount
@@ -31,7 +31,7 @@ export function useScrollState(container: Ref<HTMLElement>) {
   });
 
   onUnmounted(() => {
-    container.value.removeEventListener('scroll', updateScrollPosition);
+    target.value.removeEventListener('scroll', updateScrollPosition);
     window.removeEventListener('resize', updateContainerSize);
   });
 
