cd012a0d9e18d4a981fccd6b376b1b715fc2fae9
Miquel Romero Sanfeliu <miquelromero@outlook.com>
2020-04-26 22:00:43 +0200

    Add useKeyboard function

---
+++ src/use/keyboard.ts
@@ -0,0 +1,15 @@
+import { useEventListener } from '@vueuse/core';
+import { Key } from 'ts-key-enum';
+
+type EventType = 'keyup' | 'keydown';
+type Bindings = {
+  [k in Key]?: () => void;
+};
+
+export function useKeyboard(bindings: Bindings, eventType: EventType = 'keyup') {
+  useEventListener(eventType, (event: KeyboardEvent) => {
+    if (bindings[event.key] != null) {
+      bindings[event.key]();
+    }
+  });
+}
